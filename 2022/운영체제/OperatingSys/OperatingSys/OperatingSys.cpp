5f75ae057f7599d27f1224ac6dc39807af51cd12
ChoiHyunSan <37374613+ChoiHyunSan@users.noreply.github.com>
2022-03-11 20:04:45 +0900

    2022
    2022

---
+++ 2022/운영체제/OperatingSys/OperatingSys/OperatingSys.cpp
@@ -0,0 +1,29 @@
+﻿// OperatingSys.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+
+int main()
+{
+	printf("Hello World! \n");
+
+	int number;
+
+	while (true) {
+		printf("Enter number: ");
+		scanf_s("%d", &number);
+
+		switch (number)
+		{
+		case 1:
+			printf("1 \n");
+			break;
+		case 2:
+			printf("2 \n");
+			break;
+		default:
+			printf("error \n");
+			break;
+		}
+	}
+}
+
